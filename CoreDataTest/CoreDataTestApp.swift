4bda24fcc87db4f8f3dc238e805d08f2b4cc44c8
John DaCorte <john.dacorte@gmail.com>
2022-01-25 21:24:04 -0500

    Initial Commit

---
+++ CoreDataTest/CoreDataTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoreDataTestApp.swift
+//  CoreDataTest
+//
+//  Created by John DaCorte on 1/25/22.
+//
+
+import SwiftUI
+
+@main
+struct CoreDataTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
